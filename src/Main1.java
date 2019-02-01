28c60b738d1dd3a0973f45927cdff86e5b074961
Dominik.Wosinski <bl@wp.pl>
2019-02-02 00:36:55 +0100

    Dodane zadania

---
+++ src/Main1.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main1 {
+    public static void main(String[] args) {
+        List<Integer> integerList = Arrays.asList(1, 6, 11, 56, 22, -1 ,55, -5, 0, 100, 511, 55);
+
+    }
+}
---
+++ src/Main2.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main2 {
+    public static Set<Integer> numbers = new HashSet<>();
+
+    public static void main(String[] args) {
+
+    }
+}
